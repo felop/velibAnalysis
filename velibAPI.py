7cc291cea974763be29bed4ba86fbec8a243e70d
Felox <contact@felixlop.com>
2022-01-02 14:52:08 +0100

    added velibApi.py

---
+++ velibAPI.py
@@ -0,0 +1,13 @@
+import json
+from datetime import datetime
+
+def getLogData(logId):
+    logPath = "data/logs/log_"+str(logId)+".json"
+    logData = json.load(open(logPath, "r"))
+    logTimeStamp = logData["lastUpdatedOther"]
+    logData = {station["station_id"]:[station["numBikesAvailable"],station["num_bikes_available_types"],station["numDocksAvailable"],station["is_installed"],station["is_returning"],station["is_renting"]] for station in logData["data"]["stations"]}
+    return logData,logTimeStamp
+
+def EpochToDate(epochTime,year=0,month=0,day=1,hour=1,minute=1,second=0):
+    formatString = ":".join(list(filter(len,['%Y'*year,'%m'*month,'%d'*day,'%H'*hour,'%M'*minute,'%S'*second])))
+    return datetime.fromtimestamp(epochTime).strftime(formatString)
