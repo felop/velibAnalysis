21eafa1969e5295fa16f2c1b8d2459cf61c22523
Felox <contact@felixlop.com>
2022-01-03 23:44:43 +0100

    added video making tool

---
+++ cv2Video.py
@@ -0,0 +1,20 @@
+import cv2,os
+import numpy as np
+from glob import glob
+from tqdm import tqdm
+
+img_array = []
+images = sorted(glob('generatedMaps/TerminalOccupancies/*.png'), key=os.path.getmtime)
+
+for filename in images:
+    img = cv2.imread(filename)
+    height, width, layers = img.shape
+    size = (width,height)
+    img_array.append(img)
+
+
+out = cv2.VideoWriter('generatedMaps/TerminalOccupancies/video.avi',cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
+
+for i in tqdm(range(len(img_array))):
+    out.write(img_array[i])
+out.release()
