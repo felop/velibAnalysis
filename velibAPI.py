af81b70eeef1c138bd0c0f4f2b4e4921b441f697
Felox <contact@felixlop.com>
2022-02-08 08:54:09 +0100

    created sortSelect()

--- velibAPI.py
+++ velibAPI.py
@@ -1,16 +1,35 @@
 import json
 from datetime import datetime
 
-def getLogData(logId):
-    logPath = "../data/logs/log_"+str(logId)+".json"
+#######################################
+#### IMPORTANT :  BUILD ORDER FUNC ####
+#######################################
+
+defaultData = [0,[{'mechanical':0},{'ebike':0}],0,1,0,0]
+stationsInfos = lambda StationsInfos : {station["station_id"]:[station["numBikesAvailable"],station["num_bikes_available_types"],station["numDocksAvailable"],station["is_installed"],station["is_returning"],station["is_renting"]] for station in StationsInfos["data"]["stations"]}
+stationsList = json.load(open("stationsList.json", "r"))
+
+def getLogData(logId, logPath : str):
+    logPath = logPath+"log_"+str(logId)+".json"
     logData = json.load(open(logPath, "r"))
     logTimeStamp = logData["lastUpdatedOther"]
-    logData = {station["station_id"]:[station["numBikesAvailable"],station["num_bikes_available_types"],station["numDocksAvailable"],station["is_installed"],station["is_returning"],station["is_renting"]] for station in logData["data"]["stations"]}
+    logData = stationsInfos(logData)
     return logData,logTimeStamp
 
 def EpochToDate(epochTime,year=0,month=0,weekday=0,day=1,hour=1,minute=1,second=0):
     formatString = ":".join(list(filter(len,['%Y'*year,'%m'*month,'%w'*weekday,'%d'*day,'%H'*hour,'%M'*minute,'%S'*second])))
     return datetime.fromtimestamp(epochTime).strftime(formatString)
+
+def sortSelect(logData):
+    global stationsList, defaultData
+    newLog, fails = {}, []
+    for station in stationsList:
+        if station in logData:
+            newLog[station] = logData[station]
+        else:
+            fails.append(station)
+            newLog[station] = defaultData
+    return newLog
 
 def dataToMlArray(rawData):
     logData = {}
