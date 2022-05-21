b7fecae505b17575404a2ed63e1e95ba5c1e7c9d
Felox <contact@felixlop.com>
2022-05-21 13:11:51 +0200

    full predictor

--- ML/predictor.py
+++ ML/predictor.py
@@ -1,4 +1,4 @@
-import keras, json, urllib3, time
+import keras, json, urllib3, time, math
 from keras.models import load_model
 import numpy as np
 from datetime import datetime
@@ -19,20 +19,26 @@ def dataToMlArray(rawData):
 
 http = urllib3.PoolManager()
 data = http.request("GET", "https://velib-metropole-opendata.smoove.pro/opendata/Velib_Metropole/station_status.json")
-data = json.loads(data.data)
+rawData = json.loads(data.data)
 
 # META DATA
-date = datetime.fromtimestamp(int(data["lastUpdatedOther"])).strftime('%w:%H:%M').split(":")
+date = datetime.fromtimestamp(int(rawData["lastUpdatedOther"])).strftime('%w:%H:%M').split(":")
 weekday, hour, minutes = round(int(date[0])/7, 3), round(int(date[1])/24, 3), round(int(date[2])/60, 3)
 meta = [weekday,hour,minutes]
 # META DATA
 
 # VELIB DATA
-data = dataToMlArray(stationsInfos(data))
-data = [data[station] for station in data]
+data = stationsInfos(rawData)
+MLdata = dataToMlArray(data)
+MLdata = [MLdata[station] for station in MLdata]
 # VELIB DATA
 
-input = np.array([data[:1436]])
+input = np.array([MLdata[:1436]])
 model = load_model("test2.h5")
 prediction = model.predict(input)[0][0]*100
-print(prediction)
+
+currentStats = [data[27033125][0], data[27033125][2]]
+prediction = math.floor(prediction/100*(currentStats[0]+currentStats[1]))
+prediction = [prediction, (currentStats[0]+currentStats[1])-prediction]
+#
+print(prediction,currentStats)
