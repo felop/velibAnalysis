0d1e50912b9dcedf473e26ed4b4bc70f2ae519e8
felop <feloxlopez05@gmail.com>
2022-08-07 18:25:36 +0200

    added 'starting' logging func.

--- getData.py
+++ getData.py
@@ -2,22 +2,26 @@ import urllib3, json, os, time, datetime
 from glob import glob
 
 def reportLog(currentLog="",prevLog="",level=0,info="-"):
-    level = ["INFO", "WARNING", "ERROR", "CRITICAL"][level]
-    currentDate, prevDate, timeGap = "-", "-", "-"
-    if isinstance(currentLog, int) and currentLog != 0:
-        currentTime = datetime.datetime.fromtimestamp(currentLog)
-        currentDate = currentTime.strftime("%d/%m/%Y %H:%M:%S ")
-    else:
-        currentLog = ""
-    if isinstance(prevLog, int) and prevLog != 0:
-        prevTime = datetime.datetime.fromtimestamp(prevLog)
-        prevDate = prevTime.strftime("%d/%m/%Y %H:%M:%S ")
-    else:
-        prevLog = ""
-    if currentDate != "-" and prevDate != "-":
-        timeGap = (currentTime-prevTime)
+    if level < 4:
+        level = ["INFO", "WARNING", "ERROR", "CRITICAL"][level]
+        currentDate, prevDate, timeGap = "-", "-", "-"
+        if isinstance(currentLog, int) and currentLog != 0:
+            currentTime = datetime.datetime.fromtimestamp(currentLog)
+            currentDate = currentTime.strftime("%d/%m/%Y %H:%M:%S ")
+        else:
+            currentLog = ""
+        if isinstance(prevLog, int) and prevLog != 0:
+            prevTime = datetime.datetime.fromtimestamp(prevLog)
+            prevDate = prevTime.strftime("%d/%m/%Y %H:%M:%S ")
+        else:
+            prevLog = ""
+        if currentDate != "-" and prevDate != "-":
+            timeGap = (currentTime-prevTime)
 
-    log = f"[ {level} ] {currentDate}{currentLog} ; {prevDate}{prevLog} ; {timeGap} ; {info} \n"
+        log = f"[ {level} ] {currentDate}{currentLog} ; {prevDate}{prevLog} ; {timeGap} ; {info} \n"
+    else:
+        if level==4:
+            log=f"[ starting ] {datetime.datetime.utcnow().strftime('%d/%m/%Y %H:%M:%S ')}{datetime.datetime.fromtimestamp(int((datetime.datetime.utcnow()-datetime.datetime(1970,1,1)).total_seconds()))}\n";
     with open("temporaryStorageA.log", "a") as logFileA, open("temporaryStorageB.log", "a") as logFileB:
         logFileA.write(log);logFileB.write(log)
 
@@ -41,6 +45,7 @@ def getDataApi(lastLogId, logId):
 http = urllib3.PoolManager()
 path = "data/"
 
+reportLog(level=4)
 files = glob(path+"*")
 if len(files) == 0:
     data,logId = getDataApi(0,0)
@@ -48,8 +53,7 @@ if len(files) == 0:
         json.dump(data, file)
     reportLog(logId)
     files = glob(path+"*")
-    lastLogNumber = 0
-    lastLogId = logId
+    lastLogNumber, lastLogId = 0, logId
 else:
     lastLogNumber = max([int(files[id].split("_")[1].split(".")[0]) for id in range(len(files))])
     lastLogId = json.load(open(path+"log_"+str(lastLogNumber)+".json", "r"))["lastUpdatedOther"]
