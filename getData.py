065660a1df5a90a897898131f4fa887c6cde3135
felop <feloxlopez05@gmail.com>
2022-08-11 12:44:05 +0200

    added Velib Class

--- getData.py
+++ getData.py
@@ -1,6 +1,45 @@
 import urllib3, json, os, time, datetime, requests
 from glob import glob
 
+class Velib(object):
+    def __init__(self, id, meta=None):
+        self.data = None
+        self.id = id
+        self.timeStamp = 0
+        self.time    = None
+        self.utcTime = None
+        self.timeSinceLastUpdate = 0
+
+        if id != 0:
+            self._id = id-1
+            if meta:
+                self._timeStamp = json.load(open(paths["data"]+f"log_{self._id}.json", "r"))["lastUpdatedOther"]
+                self._time = datetime.datetime.fromtimestamp(self._timeStamp)
+            else:
+                self._timeStamp = meta[0]
+                self._time = meta[1]
+
+    def downloadData(self, http, lastLogTimeStamp):
+        while lastLogTimeStamp >= logId:
+            try:
+                self.data = json.loads(http.request("GET", "https://velib-metropole-opendata.smoove.pro/opendata/Velib_Metropole/station_status.json").data)
+            except Exception as error:
+                ##############
+                reportLog(logId,lastLogTimeStamp,3,f"failed request : {error}",logNumber=lastLogNumber+1)
+                ##############
+                time.sleep(10)
+            else:
+                self.data = json.loads(http.request("GET", "https://velib-metropole-opendata.smoove.pro/opendata/Velib_Metropole/station_status.json").data)
+                self.timeStamp = self.data["lastUpdatedOther"]
+                self.time    = datetime.datetime.fromtimestamp(self.timeStamp)
+                self.utcTime = datetime.datetime.fromtimestamp(int((datetime.datetime.utcnow()-datetime.datetime(1970,1,1)).total_seconds()))
+                self.timeSinceLastUpdate = (self.utcTime-self.time).total_seconds()
+            if self.timeSinceLastUpdate > 120.0:
+                ##############
+                reportLog(logId,lastLogTimeStamp,2,f"skip : {int(timeSinceLastUpdate/60)}",logNumber=lastLogNumber+1)
+                ##############
+                time.sleep(59)
+
 def reportLog(currentLog="",prevLog="",level=0,info="-",logNumber=""):
     if level < 4:
         if level == 3:
@@ -46,10 +85,11 @@ def getDataApi(lastLogId, logId, lastLogNumber):
                 time.sleep(59)
     return data,logId
 
-http = urllib3.PoolManager()
-path = "data/"
-iftttKey = open("iftttKey.txt", "r").read()
+paths = {"data":"/home/user0/data/", "ifttt":"/root/iftttKey.txt", "loggingFileA":"/home/user0/temporaryStorageA.log", "loggingFileB":"/root/temporaryStorageB.log"}
+iftttKey = open(paths["ifttt"], "r").read()
 files = glob(path+"*")
+http = urllib3.PoolManager()
+
 if len(files) == 0:
     lastLogNumber = -1
     data,logId = getDataApi(0,0,lastLogNumber)
